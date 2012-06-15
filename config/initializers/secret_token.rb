28d3021a668acb45f1934fc620de52a99ff23ef1
HumbleLivelyBear2 <jackmodo2012@gmail.com>
2012-06-15 15:32:33 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XSport::Application.config.secret_token = '295f102716b3296689f513d8c77254fee94f95e50e10a009acc960fc3cc31f29c2243dab5d255be505b1bfbfc83c555e739ca5d2af4aa020c818c2930833b017'
